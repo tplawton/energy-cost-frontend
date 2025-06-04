291504652d587eb6bbe37c6dc0282de5b117dfdc
Thomas Lawton <tplawton@bu.edu>
2025-06-04 13:16:31 -0400

    added options

---
+++ src/options.js
@@ -0,0 +1,12 @@
+const OPTIONS = {
+state_postal: ["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"],
+BA_climate: ["Cold", "Hot-Dry", "Hot-Humid", "Marine", "Mixed-Dry", "Mixed-Humid", "Subarctic", "Very-Cold"],
+IECC_climate_code: ["1A", "2A", "2B", "3A", "3B", "3C", "4A", "4B", "4C", "5A", "5B", "6A", "6B", "7A", "7AK", "7B", "8AK"],
+TYPEHUQ: ["1 - Mobile Home", "2 - Single-Family Detached", "3 - Single-Family Attached", "4 - Apartment (2-4 units)", "5 - Apartment (5+ units)"],
+YEARMADERANGE: ["1 - Before 1950", "2 - 1950-1959", "3 - 1960-1969", "4 - 1970-1979", "5 - 1980-1989", "6 - 1990-1999", "7 - 2000-2009", "8 - 2010-2015", "9 - 2016 or later"],
+FUELHEAT: ["1 - Electricity", "2 - Natural Gas", "3 - Fuel Oil", "5 - Wood", "7 - Other", "99 - Not reported"],
+WALLTYPE: ["1 - Brick", "2 - Stucco", "3 - Siding/Wood", "4 - Vinyl", "5 - Concrete Block", "6 - Stone", "7 - Other", "99 - Not reported"],
+OVEN: ["0 - None", "1 - Electric", "2 - Gas", "3 - Other"]
+};
+
+export default OPTIONS;
