b36cb91b780c73d42d87137946d9e5b006a04921
Thomas Lawton <tplawton@bu.edu>
2025-06-04 15:13:25 -0400

    Updates

---
+++ precache-manifest.b4ae50b10b671ab133b0a8c9c84ac996.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f22e600b3799a640986b932c252ab072",
+    "url": "/energy-cost-frontend/index.html"
+  },
+  {
+    "revision": "14e88cad29b28a25de82",
+    "url": "/energy-cost-frontend/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "ed023ef4afd521a3fc0c",
+    "url": "/energy-cost-frontend/static/js/2.f25ce426.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/energy-cost-frontend/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "14e88cad29b28a25de82",
+    "url": "/energy-cost-frontend/static/js/main.40cd0faa.chunk.js"
+  },
+  {
+    "revision": "cb39e41dc1eb22f1e26c",
+    "url": "/energy-cost-frontend/static/js/runtime~main.7bdfa4ec.js"
+  }
+]);
